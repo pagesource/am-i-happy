7543f320572af724f464516d36d24ab81af376db
Navin Tyagi <navtyagi@publicisgroupe.net>
2019-05-04 11:53:20 +0530

    am i happy app with MongoDB +Express + ejs

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # am-i-happy
 App to collect Happiness data and maintaining Happiness data with reports.
+
+To run app use follow steps
+
+#Software needed 
+1. Node 
+2. MongoDB
+
+# To run app 
+1. Take pull of latest code 
+2. Run npm i or npm install 
+3. import moodtypes.json collection in mongoDB in HappyIndex Database
+
+
+
+
---
+++ index.js
@@ -0,0 +1,55 @@
+var express = require("express");
+var app = express();
+var port = 8000;
+var bodyParser = require("body-parser");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+app.set('views', __dirname+"/views");
+app.use(express.static(__dirname));
+const mongoose = require("mongoose");
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost:27017/HappyIndex");
+const EmplyeeSchema = new mongoose.Schema({
+  name: String,
+  mood_type: String,
+  currentDate: Date,
+  message: String
+});
+const MoodSchema = new mongoose.Schema({
+  mood_type: String,
+  mood_id: String
+});
+
+
+var Employee = mongoose.model("Employee", EmplyeeSchema);
+var MoodType = mongoose.model("MoodType", MoodSchema);
+
+
+app.get("/", (req, res) => {
+    MoodType.find({},function(err, moodtypes) {
+        console.log('Mood type', moodtypes)
+        res.render("index",{moodtypes:moodtypes});
+    })
+  
+});
+
+app.post("/addHappyIndex", (req, res) => {
+  var employee = req.body;
+  employee.currentDate = new Date();
+  console.log(employee);
+  var myData = new Employee(employee);
+  myData
+    .save()
+    .then(item => {
+     res.redirect("/")
+    
+    })
+    .catch(err => {
+      res.status(400).send("Unable to save to database");
+    });
+});
+
+app.listen(port, () => {
+  console.log("Server listening on port " + port);
+});
